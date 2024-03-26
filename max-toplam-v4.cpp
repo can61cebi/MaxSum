5ea6c8802d7cfc844b057fa0ac34497985f9da29
Can Cebi <can61cebi@gmail.com>
2024-03-26 15:08:53 +0300

    Düzenleme: Segmentation Fault kasıtlı olarak alınması sağlandı.

--- max-toplam-v4.cpp
+++ max-toplam-v4.cpp
@@ -2,77 +2,80 @@
 #include <random>
 #include <algorithm>
 #include <cstdlib>
-#include <numeric>
 
 using namespace std;
 
+
 void func1() {
+
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    int* arr = (int*)malloc(10000000 * sizeof(int));
+    int* arr = {0};
 
-    for(int n = 0; n < 10000000; ++n) {
-        arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 9000000));
+    for(int n = 0; n < 1000000; ++n) {
+        arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 90000000));
     }
-
-    int n = 10000000;
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
     int max = *max_element(arr, arr + n);
     int maxrounded = round(max);
 
-    cout << "\nv1\nMax Element = " << max;
+    //cout << "\nv1\nMax Element = " << max;
 
     int total = 0;
 
-    for (int k = 0; k < n; k++) {
+    for (int k = 0; k < 10000; k++) {
         total += arr[k];
     }
 
-    cout << "\n1'den Max degere kadar olan toplam = " << (maxrounded * (maxrounded + 1)) / 2;
+    //cout << "\n1'den Max degere kadar olan toplam = " << (maxrounded * (maxrounded + 1)) / 2;
 
-    cout << "\nToplam: " << total << "\n";
-
-    free(arr);
+    //cout << "\nToplam: " << total << "\n";
+    
 }
 
 void func2() {
+
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    int* arr = (int*)malloc(1000000 * sizeof(int));
+    int arr[10000000] = {0};
 
     for(int n = 0; n < 1000000; ++n) {
-        arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 9000000));
+        arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 90000000));
     }
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int n = 1000000;
-
-    long long sum = 0;
+    int sum = 0;
 
     for (int i = 0; i < n; i++) {
-        sum += arr[i];
+        sum = sum + arr[i];
     }
-
+    
     int avg = sum / n;
-    cout << "\nv2\nOrtalama (bolme islemiyle): " << avg;
+    //cout << "\nv2\nOrtalama (bolme islemiyle): " << avg;
 
-    long long sumfunc = accumulate(arr, arr + n, 0LL);
+    int sumfunc = accumulate(arr, arr + n, 0.0f);
     int avgfunc = sumfunc / n;
 
-    cout << "\nOrtalama (fonksiyonla): " << avgfunc;
+    //cout << "\nOrtalama (fonksiyonla): " << avgfunc;
 
-    long long onetoavg = 0;
+    int onetoavg = 0;
 
     for (int i = 1; i <= avgfunc; i++) {
         onetoavg += i;
     }
+    
+    //cout << "1'den Ortalamaya kadar olan sayıların toplamı (1 eklemeli): " << onetoavg << "\n";
 
-    cout << "\n1'den Ortalamaya kadar olan sayıların toplamı: " << onetoavg << "\n";
-
-    free(arr);
 }
 
 int main() {
-    for (int i = 0; i < 100; i++) {
+
+    for (int i = 0; i < 100; i++)
+    {
         func1();
         func2();
     }
+    
 }
