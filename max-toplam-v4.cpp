d677b9fa88a719fff3d770fa4d2dd1e8b202b93b
Can Cebi <can61cebi@gmail.com>
2024-03-31 03:33:59 +0300

    Düzenleme: SIGSEGV hatası ekrana yazdirilacak, henuz tamamlanmadi.

--- max-toplam-v4.cpp
+++ max-toplam-v4.cpp
@@ -2,15 +2,32 @@
 #include <random>
 #include <algorithm>
 #include <cstdlib>
+#include <cstring>
+#include <csignal>
+#include <exception>
+#include <unistd.h>
 
 using namespace std;
 
+class SignalException : public exception {
+public:
+    const char* what() const throw () {
+        return "Signal received!";
+    }
+};
+
+void signalHandler(int signum) {
+    const char* message = "Critical error signal received. Program will terminate.\n";
+
+    write(STDERR_FILENO, message, strlen(message));
+    _exit(EXIT_FAILURE);
+}
 
 void func1() {
 
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    int* arr = {0};
+    int arr[10000000] = {0};
 
     for(int n = 0; n < 1000000; ++n) {
         arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 90000000));
@@ -20,18 +37,11 @@ void func1() {
     int max = *max_element(arr, arr + n);
     int maxrounded = round(max);
 
-    //cout << "\nv1\nMax Element = " << max;
-
     int total = 0;
 
     for (int k = 0; k < 10000; k++) {
         total += arr[k];
     }
-
-    //cout << "\n1'den Max degere kadar olan toplam = " << (maxrounded * (maxrounded + 1)) / 2;
-
-    //cout << "\nToplam: " << total << "\n";
-    
 }
 
 void func2() {
@@ -53,29 +63,33 @@ void func2() {
     }
     
     int avg = sum / n;
-    //cout << "\nv2\nOrtalama (bolme islemiyle): " << avg;
 
     int sumfunc = accumulate(arr, arr + n, 0.0f);
     int avgfunc = sumfunc / n;
-
-    //cout << "\nOrtalama (fonksiyonla): " << avgfunc;
 
     int onetoavg = 0;
 
     for (int i = 1; i <= avgfunc; i++) {
         onetoavg += i;
     }
-    
-    //cout << "1'den Ortalamaya kadar olan sayıların toplamı (1 eklemeli): " << onetoavg << "\n";
 
 }
 
 int main() {
+    signal(SIGINT, signalHandler);
+    signal(SIGSEGV, signalHandler);
 
-    for (int i = 0; i < 100; i++)
-    {
-        func1();
-        func2();
+    try {
+        for (int i = 0; i < 100; i++) {
+            func1();
+            func2();
+        }
+    } catch (const SignalException& e) {
+        cout << "SignalException caught: " << e.what() << endl;
+        return EXIT_FAILURE;
+    } catch (...) {
+        cout << "An unexpected exception occurred." << endl;
     }
-    
+
+    return EXIT_SUCCESS;
 }
