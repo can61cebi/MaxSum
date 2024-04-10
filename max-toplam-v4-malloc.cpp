6504e10336d9aa2b61e609cdfce73ab60e289db6
Can Cebi <can61cebi@gmail.com>
2024-04-10 16:20:58 +0300

    düzenleme: v5 için son halde bırakıldı.

--- max-toplam-v4-malloc.cpp
+++ max-toplam-v4-malloc.cpp
@@ -19,17 +19,11 @@ void func1() {
     int max = *max_element(arr, arr + n);
     int maxrounded = round(max);
 
-    //cout << "\nv1\nMax Element = " << max;
-
     int total = 0;
 
     for (int k = 0; k < n; k++) {
         total += arr[k];
     }
-
-    //cout << "\n1'den Max degere kadar olan toplam = " << (maxrounded * (maxrounded + 1)) / 2;
-
-    //cout << "\nToplam: " << total << "\n";
 
     free(arr);
 }
@@ -52,20 +46,15 @@ void func2() {
     }
 
     int avg = sum / n;
-    //cout << "\nv2\nOrtalama (bolme islemiyle): " << avg;
 
     long long sumfunc = accumulate(arr, arr + n, 0LL);
     int avgfunc = sumfunc / n;
-
-    //cout << "\nOrtalama (fonksiyonla): " << avgfunc;
 
     long long onetoavg = 0;
 
     for (int i = 1; i <= avgfunc; i++) {
         onetoavg += i;
     }
-
-    //cout << "\n1'den Ortalamaya kadar olan sayıların toplamı: " << onetoavg << "\n";
 
     free(arr);
 }
