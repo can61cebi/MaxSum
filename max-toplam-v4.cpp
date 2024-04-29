9c885556af6dcd7918b85bf9b49f0f1a7dfa07b2
Can Cebi <can61cebi@gmail.com>
2024-04-29 17:33:39 +0300

    V4 standart array ve malloc ile tasarlandi.

--- max-toplam-v4.cpp
+++ max-toplam-v4.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <cstdlib>
 #include <numeric>
-#include <ctime>
 
 using namespace std;
 
@@ -16,12 +15,13 @@ void func1() {
         arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 9000000));
     }
 
-    int max = *max_element(arr, arr + 10000000);
+    int n = 10000000;
+    int max = *max_element(arr, arr + n);
     int maxrounded = round(max);
 
-    long long total = 0;
+    int total = 0;
 
-    for (int k = 0; k < 10000000; k++) {
+    for (int k = 0; k < n; k++) {
         total += arr[k];
     }
 
@@ -29,22 +29,26 @@ void func1() {
 }
 
 void func2() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
     int* arr = (int*)malloc(10000000 * sizeof(int));
 
     for(int n = 0; n < 10000000; ++n) {
         arr[n] = 1000000 + static_cast<int>(rand()) / (static_cast<int>(RAND_MAX / 9000000));
     }
 
+    int n = 10000000;
+
     long long sum = 0;
 
-    for (int i = 0; i < 10000000; i++) {
+    for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
 
-    int avg = sum / 10000000;
+    int avg = sum / n;
 
-    long long sumfunc = accumulate(arr, arr + 10000000, 0LL);
-    int avgfunc = sumfunc / 10000000;
+    long long sumfunc = accumulate(arr, arr + n, 0LL);
+    int avgfunc = sumfunc / n;
 
     long long onetoavg = 0;
 
@@ -56,7 +60,6 @@ void func2() {
 }
 
 int main() {
-    srand(static_cast<unsigned int>(time(nullptr)));
     for (int i = 0; i < 100; i++) {
         func1();
         func2();
